a14739540d6f8df70d0e8eef716841a60f9ab691
Leandro <lihermann@outlook.com>
2023-06-03 22:20:15 -0300

    Add InvalidParameterError class

--- server/src/common/errors.ts
+++ server/src/common/errors.ts
@@ -1,12 +1,13 @@
 export class NotFoundError extends Error {
-  name = 'Not found';
-  message: string;
-
-  /**
-   *
-   */
   constructor(entity: string) {
     super();
     this.message = `The ${entity} with the given ID was not found`;
+  }
+}
+
+export class InvalidParameterError extends Error {
+  constructor(public parameterName: string) {
+    super();
+    this.message = `Incorrect value for the ${parameterName} parameter`;
   }
 }
--- server/src/middleware/error.middleware.ts
+++ server/src/middleware/error.middleware.ts
@@ -1,12 +1,12 @@
 import { Express, NextFunction, Request, Response } from 'express';
-import { NotFoundError } from '../common/errors';
+import { InvalidParameterError, NotFoundError } from '../common/errors';
 
 export function errorMiddleware(app: Express): void {
   app.use((error: any, req: Request, res: Response, next: NextFunction) => {
     if (error instanceof NotFoundError) {
-      res.status(404).json({ error: error.message });
-    }
-
-    res.status(500).json({ error: 'Internal Server Error' });
+      res.status(404).send({ error: error.message });
+    } else if (error instanceof InvalidParameterError) {
+      res.status(400).send({ error: error.message });
+    } else res.status(500).json({ error: 'Internal Server Error' });
   });
 }
