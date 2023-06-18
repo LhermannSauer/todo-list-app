07c3e044ae98f9b1786edca65869f8db1e40b13c
Leandro <lihermann@outlook.com>
2023-06-18 11:12:13 -0300

    remove if else statements in error middleware "

--- server/src/middleware/error.middleware.ts
+++ server/src/middleware/error.middleware.ts
@@ -5,8 +5,12 @@ export function errorMiddleware(app: Express): void {
   app.use((error: any, req: Request, res: Response, next: NextFunction) => {
     if (error instanceof NotFoundError) {
       res.status(404).send({ error: error.message });
-    } else if (error instanceof InvalidParameterError) {
+    }
+
+    if (error instanceof InvalidParameterError) {
       res.status(400).send({ error: error.message });
-    } else res.status(500).json({ error: 'Internal Server Error' });
+    }
+
+    res.status(500).json({ error: 'Internal Server Error' });
   });
 }
