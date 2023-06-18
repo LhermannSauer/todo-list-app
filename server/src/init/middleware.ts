5c4a791d72d23763a03f902c42343eaba357dc31
Leandro <lihermann@outlook.com>
2023-06-18 10:49:52 -0300

    added CORS to middleware

--- server/src/init/middleware.ts
+++ server/src/init/middleware.ts
@@ -1,6 +1,7 @@
 import { Express, json, urlencoded } from 'express';
 import { errorMiddleware } from '../middleware/error.middleware';
 import { routesMiddleware } from '../middleware/routes.middleware';
+import cors from 'cors';
 
 // Single file where all middleware is applied to the app
 
@@ -8,7 +9,7 @@ export function initMiddleware(app: Express): void {
   // express middleware
   app.use(json());
   app.use(urlencoded({ extended: false }));
-
+  app.use(cors());
   // app middleware
   routesMiddleware(app);
   errorMiddleware(app);
